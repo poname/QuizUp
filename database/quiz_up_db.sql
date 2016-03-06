ac7c64cf803e3aeb3b5f97352cbb7f730fa0ce58
Peyman Zeynali <peymanzeynali@yahoo.com>
2016-03-06 21:47:22 +0330

    database

---
+++ database/quiz_up_db.sql
@@ -0,0 +1,92 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost_3306
+Source Server Version : 50628
+Source Host           : localhost:3306
+Source Database       : quiz_up
+
+Target Server Type    : MYSQL
+Target Server Version : 50628
+File Encoding         : 65001
+
+Date: 2016-03-05 23:10:40
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for country
+-- ----------------------------
+DROP TABLE IF EXISTS `country`;
+CREATE TABLE `country` (
+  `cid` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`cid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of country
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for question
+-- ----------------------------
+DROP TABLE IF EXISTS `question`;
+CREATE TABLE `question` (
+  `qid` int(11) NOT NULL AUTO_INCREMENT,
+  `cid` int(11) NOT NULL,
+  `description` text NOT NULL,
+  `ans1` text NOT NULL,
+  `ans2` text NOT NULL,
+  `ans3` text NOT NULL,
+  `ans4` text NOT NULL,
+  PRIMARY KEY (`qid`),
+  KEY `question_to_category` (`cid`),
+  CONSTRAINT `question_to_category` FOREIGN KEY (`cid`) REFERENCES `question_category` (`cid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of question
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for question_category
+-- ----------------------------
+DROP TABLE IF EXISTS `question_category`;
+CREATE TABLE `question_category` (
+  `cid` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`cid`),
+  KEY `question_category_to_user` (`user_id`),
+  CONSTRAINT `question_category_to_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of question_category
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `family` varchar(255) NOT NULL,
+  `gender` enum('MALE','FEMALE') NOT NULL DEFAULT 'MALE',
+  `password` varchar(255) NOT NULL,
+  `is_active` tinyint(1) NOT NULL,
+  `verification_code` char(64) NOT NULL,
+  `cid` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `unique_emails` (`email`) USING HASH,
+  KEY `user_to_country` (`cid`),
+  CONSTRAINT `user_to_country` FOREIGN KEY (`cid`) REFERENCES `country` (`cid`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
