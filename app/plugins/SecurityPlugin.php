94484576222da23fb4269dd12deb3527ce93cd52
Peyman Zeynali <peymanzeynali@yahoo.com>
2016-04-15 20:07:23 +0430

    ranking added for first test1

--- app/plugins/SecurityPlugin.php
+++ app/plugins/SecurityPlugin.php
@@ -39,7 +39,8 @@ class SecurityPlugin extends Plugin
         $privateResources = array(
             'category' => array('index', 'list', 'delete', 'edit', 'create'),
             'question'     => array('index', 'create', 'list', 'edit', 'delete'),
-            'quiz' => array('selectCategory','do','answer')
+            'quiz' => array('selectCategory','do','answer'),
+            'ranking' => array('index','more')
         );
         foreach ($privateResources as $resource => $actions) {
             $acl->addResource(new Resource($resource), $actions);
