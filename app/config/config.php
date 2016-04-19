5c961bfab75c5f553f913bc10a4859bb8e6d2aff
Mohammad Nazari <mohammad.nazari92@gmail.com>
2016-04-19 20:13:35 +0430

    new configuration

--- app/config/config.php
+++ app/config/config.php
@@ -32,5 +32,6 @@ return new \Phalcon\Config(array(
     'quizup' => array(
         'question_time' => 10 , //in seconds
         'correct_answer_points' => 1 // point for every question is {{correct_answer_points}} * remaining_seconds,
+        
     )
 ));
